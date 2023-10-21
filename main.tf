c185ed2a22611183b177704023d04e246558341f
MudassirKhan22 <khanmudassir2000@gmail.com>
2023-10-21 13:16:50 +0530

    Changes

--- main.tf
+++ main.tf
@@ -6,13 +6,6 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket = "tf-backend-sample"
-    key = "terraform.tfstate"
-    region = "ap-south-1"
-    dynamodb_table = "tf-sample"
-    
-  }
 }
 
 provider "aws" {
